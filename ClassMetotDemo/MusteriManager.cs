761ca0bd2828169c13eddc8dc097ae10302d14dc
Kubilay Yılmaz <kubilayyilmaz@outlook.com.tr>
2023-09-17 22:56:37 +0300

    17.09.2023 Gün Sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi. " + musteri.Ad);
+        }
+
+        public void Guncelle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Güncellendi. " + musteri.Ad);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi. " + musteri.Ad);
+        }
+
+        public void Listele()
+        {
+            Console.WriteLine("Müşteriler Listelendi.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Kubilay";
+            musteri1.Soyad = "Yılmaz";
+            musteri1.TcKimlikNo = "141411231231";
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Aslıhan";
+            musteri2.Soyad = "Yılmaz";
+            musteri2.TcKimlikNo = "124128132121";
+
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Ad = "Kıvanç";
+            musteri3.Soyad = "Yılmaz";
+            musteri3.TcKimlikNo = "481212412312";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3};
+            
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            musteriManager.Guncelle(musteri2);
+
+            musteriManager.Sil(musteri3);
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+            }
+
+            musteriManager.Listele();
+        }
+    }
+}
